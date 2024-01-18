a093d6beb796f3eb98555d143200af224a858472
Aman Elemanov <aman.and.naima@gmail.com>
2024-01-18 14:17:04 +0000

    We added python commands for dictionaries

---
+++ 04_python_dictionaries.py
@@ -0,0 +1,14 @@
+#First look at dictionaries
+
+#A dictionary is a collection of key-value pairs
+
+#Syntax
+myDict = {
+  "Dog":"Barks",
+  "Cat":"Meows",
+  "Whale":"Shreeks"
+}
+print(myDict)
+
+#Indexing is different from lists
+print(myDict["Dog"])
